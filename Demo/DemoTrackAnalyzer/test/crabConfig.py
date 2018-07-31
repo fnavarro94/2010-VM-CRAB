39a74aefe8a83cd14349720662d29aaf8b76f5e8
Felipe Navarro <felinavarro94@gmail.com>
2018-07-31 18:31:02 +0200

    crab config file updated

--- Demo/DemoTrackAnalyzer/test/crabConfig.py
+++ Demo/DemoTrackAnalyzer/test/crabConfig.py
@@ -1,19 +1,20 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'tutorial_May2015_Data_analysis'
-config.General.workArea = 'crab_projects'
+config.General.requestName = 'jobName_crab'
+config.General.workArea = 'Example_project'
 config.General.transferOutputs = True
 config.General.transferLogs = False
-
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'DemoTrackanalyzer_cfg.py'
-config.Data.inputDataset = '/SingleElectron/Run2011A-12Oct2013-v1/AOD'
+config.JobType.psetName = 'DemoTrackanalyzer_cfg.py' 
+config.JobType.outputFiles = ['outfile.root']
+config.Data.inputDataset = '/EG/Run2010A-Apr21ReReco-v1/AOD'
+#config.Data.userInputFiles = ['root://eospublic.cern.ch//eos/opendata/cms/Run2010B/Photon/AOD/Apr21ReReco-v1/0000/0003D1EF-B071-E011-BA0A-78E7D1651098.root', 'root://eospublic.cern.ch//eos/opendata/cms/Run2010B/Photon/AOD/Apr21ReReco-v1/0000/001CE57A-F470-E011-8208-001CC4A934D8.root']
+config.Data.publication = False
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
+config.Data.unitsPerJob =  500
 
-
-config.Site.storageSite = 'T2_US_Nebraska'
+config.Site.storageSite = 'T2_US_Nebraska'  # you might need to change this to a site you have acces too
 config.section_("Debug")
-config.Debug.extraJDL = [ '+DESIRED_Sites="T3_CH_Opportunistic_opendata"','+JOB_CMSSite="T3_CH_Opportunistic_opendata"']
+
